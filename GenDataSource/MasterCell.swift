24ffaf9f1640b8a641fe7022b37a853c6a377eca
Mike Saradeth <mikesaradeth@yahoo.com>
2019-06-27 21:11:53 -0700

    init

---
+++ GenDataSource/MasterCell.swift
@@ -0,0 +1,76 @@
+//
+//  MasterCell.swift
+//  GenDataSource
+//
+//  Created by Mike Saradeth on 6/27/19.
+//  Copyright © 2019 Mike Saradeth. All rights reserved.
+//
+
+import UIKit
+
+struct Book {
+    var title: String
+    var description: String
+}
+
+class MasterCell: UICollectionViewCell {
+    static let cellIdentifier = "cell"
+    
+    lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel(frame: .zero)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.systemFont(ofSize: 21, weight: .semibold)
+        return titleLabel
+    }()
+    lazy var detailLabel: UILabel = {
+        let detailLabel = UILabel()
+        detailLabel.translatesAutoresizingMaskIntoConstraints = false
+        return detailLabel
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    
+    func configure(item: Book) {
+        titleLabel.text = item.title
+        detailLabel.text = item.description
+    }
+    
+    func setupViews() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
+        
+        contentView.addSubview(titleLabel)
+        titleLabel.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 8).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
+        titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        
+        contentView.addSubview(detailLabel)
+        detailLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        detailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
+        detailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
+        detailLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+    }
+    
+    
+    //Mark Self sizing cell
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        //calc size
+        setNeedsLayout()
+        layoutIfNeeded()
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+    
+        //set Height
+        layoutAttributes.size.height = size.height
+        
+        return layoutAttributes
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- GenDataSource/ViewController.swift
+++ GenDataSource/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .gray
+        return collectionView
+    }()
+    
+    init(dataSource: UICollectionViewDataSource) {
+        super.init(nibName: nil, bundle: nil)
+        collectionView.dataSource = dataSource
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.addSubview(collectionView)
+        collectionView.fillsuperView()
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(MasterCell.self, forCellWithReuseIdentifier: MasterCell.cellIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
